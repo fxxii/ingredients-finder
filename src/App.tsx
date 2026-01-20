ab375fdcfadfc66bba2595125cf3ba8bb22d2f24
gram <gram@fxxii.com>
2026-01-20 16:39:46 +0000

    fix: Resolve TS build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { AppLayout } from './components/AppLayout';
 import { HomePage } from './pages/HomePage';
 import { Scanner } from './components/Scanner';
--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Toaster } from 'react-hot-toast'; // Need to install react-hot-toast or build custom
+
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '../lib/queryClient';
 
